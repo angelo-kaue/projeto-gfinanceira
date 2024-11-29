0b4d3e51b94abf107b8f88bfce9169af42a7ae50
angelo-kaue <angelo.kaue@hotmail.com>
2024-11-29 14:55:44 -0300

    Finalização da funcionalidade de despesas com filtros e operações CRUD

--- lib/cfinanceiro/finance.ex
+++ lib/cfinanceiro/finance.ex
@@ -3,81 +3,74 @@ defmodule Cfinanceiro.Finance do
   alias Cfinanceiro.Repo
 
   alias Cfinanceiro.Finance.Despesa
-  alias Cfinanceiro.Finance.Receita
 
-  # Funções de Receita permanecem inalteradas.
-  def list_receitas do
-    Repo.all(Receita)
-  end
-
-  def get_receita!(id), do: Repo.get!(Receita, id)
-
-  def create_receita(attrs \\ %{}) do
-    %Receita{}
-    |> Receita.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  def update_receita(%Receita{} = receita, attrs) do
-    receita
-    |> Receita.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_receita(%Receita{} = receita) do
-    Repo.delete(receita)
-  end
-
-  def change_receita(%Receita{} = receita, attrs \\ %{}) do
-    Receita.changeset(receita, attrs)
-  end
-
-  # Alterações em despesas para suportar filtros
+  # Lista despesas com filtros e ordenações
   def list_despesas(params \\ %{}) do
     query = from d in Despesa
 
+    # Filtro por mês
     query =
-      cond do
-        params["filter_month"] ->
-          from d in query,
-            where: fragment("to_char(?, 'YYYY-MM') = ?", d.data, ^params["filter_month"])
+      if params["filter_month"] && params["filter_month"] != "" do
+        from d in query,
+          where: fragment("to_char(?, 'YYYY-MM') = ?", d.data, ^params["filter_month"])
+      else
+        query
+      end
 
-        params["start_date"] && params["end_date"] ->
-          from d in query,
-            where: d.data >= ^Date.from_iso8601!(params["start_date"]) and d.data <= ^Date.from_iso8601!(params["end_date"])
+    # Filtro por período
+    query =
+      if valid_date?(params["start_date"]) and valid_date?(params["end_date"]) do
+        from d in query,
+          where: d.data >= ^Date.from_iso8601!(params["start_date"]) and
+                 d.data <= ^Date.from_iso8601!(params["end_date"])
+      else
+        query
+      end
 
-        params["order"] == "desc" ->
-          from d in query, order_by: [desc: d.valor]
-
-        params["order"] == "asc" ->
-          from d in query, order_by: [asc: d.valor]
-
-        true ->
-          query
+    # Ordenação
+    query =
+      case params["order"] do
+        "desc" -> from d in query, order_by: [desc: d.valor]
+        "asc" -> from d in query, order_by: [asc: d.valor]
+        _ -> query
       end
 
     Repo.all(query)
   end
 
-  def get_despesa!(id), do: Repo.get!(Despesa, id)
+  defp valid_date?(nil), do: false
+  defp valid_date?(""), do: false
+  defp valid_date?(date) do
+    case Date.from_iso8601(date) do
+      {:ok, _date} -> true
+      _ -> false
+    end
+  end
 
+  # Função para criar ou alterar um changeset
+  def change_despesa(%Despesa{} = despesa, attrs \\ %{}) do
+    Despesa.changeset(despesa, attrs)
+  end
+
+  # Cria uma despesa
   def create_despesa(attrs \\ %{}) do
     %Despesa{}
-    |> Despesa.changeset(attrs)
+    |> change_despesa(attrs)
     |> Repo.insert()
   end
 
+  # Retorna uma despesa pelo ID ou lança erro se não encontrar
+  def get_despesa!(id), do: Repo.get!(Despesa, id)
+
+  # Atualiza uma despesa
   def update_despesa(%Despesa{} = despesa, attrs) do
     despesa
-    |> Despesa.changeset(attrs)
+    |> change_despesa(attrs)
     |> Repo.update()
   end
 
+  # Exclui uma despesa
   def delete_despesa(%Despesa{} = despesa) do
     Repo.delete(despesa)
-  end
-
-  def change_despesa(%Despesa{} = despesa, attrs \\ %{}) do
-    Despesa.changeset(despesa, attrs)
   end
 end
